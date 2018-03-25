93d0e7ec335f66be00da95154a0a22b5a50700ad
eric <erichfeinstein@gmail.com>
2018-03-25 14:37:24 -0400

    New file Income.java

---
+++ Income.java
@@ -0,0 +1,43 @@
+
+public class Income {
+	
+	private int id;
+	private double amount;
+	private double rollover;
+	private int payPeriod; //Number of days of pay period
+	
+	public Income(int id, double amount, double rollover, int payPeriod) {
+		this.id = id;
+		this.amount = amount;
+		this.rollover = rollover;
+		this.payPeriod = payPeriod;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public double getRollover() {
+		return rollover;
+	}
+	public void setRollover(double rollover) {
+		this.rollover = rollover;
+	}
+	public int getPayPeriod() {
+		return payPeriod;
+	}
+	public void setPayPeriod(int payPeriod) {
+		this.payPeriod = payPeriod;
+	}
+	
+	
+
+}
