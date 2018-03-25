bb81c1124e85a378ed22c57b4c43977bd62a2939
eric <erichfeinstein@gmail.com>
2018-03-25 13:02:38 -0400

    Category class basics

--- Category.java
+++ Category.java
@@ -2,6 +2,77 @@ import java.util.ArrayList;
 
 public class Category 
 {
+	private String name;
+	private double initialFunds;
+	private double currentFunds;
+	private int priority;
+	private ArrayList<Category> subcategories;
 	
+	public Category(String name, double initialFunds, int priority) {
+		this.name = name;
+		this.initialFunds = initialFunds;
+		this.currentFunds = initialFunds;
+		this.priority = priority;
+	};
+	
+	private void decreaseMoney(double amount) {
+		currentFunds -= amount;
+	}
+	
+	private void addMoney(double amount) {
+		currentFunds += amount;
+	}
+	
+	private void resetMoney() {
+		currentFunds = initialFunds;
+	}
+	
+	private void addSubcategory(Category c) {
+		subcategories.add(c);
+	}
+	
+	//private void removeSubcategory
+	
+	//Getter-setters
+	
+	private String getName() {
+		return name;
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+	
+	private double getInitialFunds() {
+		return initialFunds;
+	}
+	
+	private void setInitialFunds(double initialFunds) {
+		this.initialFunds = initialFunds;
+	}
+	
+	private double getCurrentFunds() {
+		return currentFunds;
+	}
+	
+	private void setCurrentFunds(double currentFunds) {
+		this.currentFunds = currentFunds;
+	}
+	
+	private int getPriority() {
+		return priority;
+	}
+	
+	private void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	private ArrayList<Category> getSubcategories() {
+		return subcategories;
+	}
+	
+	private void setSubcategories(ArrayList<Category> subcategories) {
+		this.subcategories = subcategories;
+	}
 	
 }
