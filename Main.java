17ef70201e651fa11de9f7264574be73819a2057
Paulumbus <36905980+Paulumbus@users.noreply.github.com>
2018-03-25 12:28:21 -0400

    Created New Classes

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main 
+{
+	int ID;
+	String email;
+	int password;
+	String firstName;
+	String lastName;
+	ArrayList<Category> categoryList = new ArrayList<Category>();
+	double initialFunds;
+	
+}
---
+++ Transaction.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+public class Transaction 
+{
+	Category transactionCategory;
+	double transactionAmount;
+	Date transactionDate;
+	String transactionNotes;
+	
+	
+}
