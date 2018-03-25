a2c079e5263c9fa54beaa0c28d9bb9044a7dbe40
Paulumbus <36905980+Paulumbus@users.noreply.github.com>
2018-03-25 13:01:53 -0400

    Added a lot to the Main Category, constructor to the Transaction Category

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Main 
 {
@@ -10,4 +12,51 @@ public class Main
 	ArrayList<Category> categoryList = new ArrayList<Category>();
 	double initialFunds;
 	ArrayList<Transaction> transactionLog = new ArrayList<Transaction>();
+	
+	public Main()
+	{
+		
+	}
+	
+	public Date checkDate()
+	{
+		return (Date)Calendar.getInstance().getTime();
+	}
+	
+	public void decreaseMoney(Category category, double amount, String note)
+	{
+		category.DecreaseMoney(amount);
+		transactionLog.add(new Transaction(category, amount, checkDate(), note));
+	}
+	
+	public void addMoney(Category category, double amount, String note)
+	{
+		category.addMoney(amount);
+		transactionLog.add(new Transaction(category, amount, checkDate(), note));
+	}
+	
+	public double rollover()
+	{
+		int rolling = 0;
+		for(Category c: categoryList)
+		{
+			rolling+= c.withdrawMoney();
+			c.resetMoney();
+		}
+		
+		return rolling;
+	}
+	
+	public void viewCurrentFunds()
+	{
+		
+	}
+	
+	public void displayLog()
+	{
+		for(Transaction t: transactionLog)
+		{
+			System.out.println("Category: " + t.transactionCategory.getName() + " --- Amount: " + t.transactionAmount + " --- Date: " + t.transactionDate + " --- Notes: " + t.transactionNotes);
+		}
+	}
 }
