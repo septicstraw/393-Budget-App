4369f502dbfac49a7c1e7c231852ce142c065e3c
Nick Verrilli <nickverrilli@gmail.com>
2018-03-27 16:23:43 -0400

    test push

--- Budget_App/src/main/java/com/ex/bgt/App.java
+++ Budget_App/src/main/java/com/ex/bgt/App.java
@@ -1,7 +1,10 @@
 package com.ex.bgt;
 
+import com.ex.bgt.domain.Category;
 import com.ex.bgt.domain.User;
+import com.ex.dao.CategoryDao;
 import com.ex.dao.UserDao;
+import com.ex.impl.CategoryDaoImpl;
 import com.ex.impl.UserDaoImpl;
 
 /**
@@ -15,6 +18,16 @@ public class App
         User usr = new User("nickverrilli@gmail.com", 88204, "Nick", "Verrilli");
         
         UserDao usrDao = new UserDaoImpl();
+        
         usrDao.saveUser(usr);
+        
+        CategoryDao catDao = new CategoryDaoImpl();
+        
+        Category cat = new Category(0, "name", 0, 0);
+        
+        catDao.saveCategory(cat);
+        
+        
+
     }
 }
