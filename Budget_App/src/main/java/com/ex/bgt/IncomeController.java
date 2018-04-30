375d2e4091d71c523eb08441c9ae5072d2eaf0b5
Nick Verrilli <nickaverrilli@gmail.com>
2018-04-30 11:30:19 -0400

    get rid of gluon theme

---
+++ Budget_App/src/main/java/com/ex/bgt/IncomeController.java
@@ -0,0 +1,28 @@
+package com.ex.bgt;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class IncomeController {
+
+    @FXML
+    void redirectCreateCat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redirectSaveTx(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redirectTx(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redirectViewCat(ActionEvent event) {
+
+    }
+
+}
